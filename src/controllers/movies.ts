927effb0dc998571b5d92901d5f78170c8eb9aad
Gimigliano <daniel@gimigliano.blog>
2022-11-23 20:55:37 -0300

    feat: ensure MoviesController returns 500 if movies service throws

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '../errors'
-import { badRequest, ok } from '../helpers/http-helper'
+import { badRequest, ok, serverError } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, Controller, MoviesService } from './movies-protocols'
 
 export class MoviesController implements Controller {
@@ -10,10 +10,14 @@ export class MoviesController implements Controller {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (request.params && typeof request.params !== 'number') {
-      return badRequest(new InvalidParamError('params'))
+    try {
+      if (request.params && typeof request.params !== 'number') {
+        return badRequest(new InvalidParamError('params'))
+      }
+      const getMovies = await this.getMovies.get(request.params)
+      return ok(getMovies)
+    } catch (e) {
+      return serverError()
     }
-    const getMovies = await this.getMovies.get(request.params)
-    return ok(getMovies)
   }
 }
