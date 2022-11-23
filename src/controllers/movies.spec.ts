e0bcdc2c89bc85755b8da1f52307ed3e2154d38d
Gimigliano <daniel@gimigliano.blog>
2022-11-23 18:48:19 -0300

    test: ensure MoviesController returns 200 if valid params is provided

--- src/controllers/movies.spec.ts
+++ src/controllers/movies.spec.ts
@@ -19,4 +19,13 @@ describe('movies controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('should return 200 if valid params provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      params: 1
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+  })
 })
