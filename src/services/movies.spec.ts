70f306b0a5c71a86d3eafac8e90777a8e3b74a35
Gimigliano <daniel@gimigliano.blog>
2022-11-24 17:31:40 -0300

    test: ensure MoviesService call MoviesService.get

--- src/services/movies.spec.ts
+++ src/services/movies.spec.ts
@@ -1,20 +1,46 @@
+import { IMoviesModel, Model } from '../usecases'
 import { MoviesService } from './movies'
 
 describe('movies service', () => {
+  const makeMoviesModel = (): Model => {
+    class MoviesModelStub implements Model {
+      async get (offset?: number): Promise<IMoviesModel[]> {
+        const fakeMovies = [{
+          id: 'valid_id',
+          offset: 'valid_offset',
+          title: 'valid_title',
+          original_title: 'valid_original_title',
+          description: 'valid_description',
+          release_date: 'valid_release_date',
+          pointing: 'valid_pointing'
+        }]
+        return new Promise(resolve => resolve(fakeMovies))
+      }
+    }
+
+    return new MoviesModelStub()
+  }
+
   interface SutTypes {
     sut: MoviesService
+    moviesModelStub: Model
   }
 
   const makeSut = (): SutTypes => {
-    const sut = new MoviesService()
+    const moviesModelStub = makeMoviesModel()
+    const sut = new MoviesService(moviesModelStub)
     return {
-      sut
+      sut,
+      moviesModelStub
     }
   }
 
   test('should return an array of objects on success', async () => {
     const { sut } = makeSut()
-    const result = await sut.get(1)
+    const offset = {
+      params: 1
+    }
+    const result = await sut.get(offset.params)
     expect(result).toEqual([{
       id: 'valid_id',
       offset: 'valid_offset',
@@ -24,5 +50,12 @@ describe('movies service', () => {
       release_date: 'valid_release_date',
       pointing: 'valid_pointing'
     }])
+  })
+
+  test('should call moviesModel', async () => {
+    const { sut, moviesModelStub } = makeSut()
+    const addSpy = jest.spyOn(moviesModelStub, 'get')
+    await sut.get()
+    expect(addSpy).toHaveBeenCalled()
   })
 })
