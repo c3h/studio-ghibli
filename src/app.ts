3cc349e5f23fdd09b22522bdfe6df41ca9b5065c
Gimigliano <daniel@gimigliano.blog>
2022-11-29 10:08:24 -0300

    feat: add cors on app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import movies from './routes/movies'
 
@@ -20,6 +21,7 @@ export default class App {
 
     this.app.use(accessControl)
     this.app.use(express.json())
+    this.app.use(cors())
   }
 
   private routes (): void {
