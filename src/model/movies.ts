a31f86a31f20c33ed20077f0bcfbd1d203fde06c
Gimigliano <daniel@gimigliano.blog>
2022-11-28 22:17:46 -0300

    refactor: corrections

--- src/model/movies.ts
+++ src/model/movies.ts
@@ -1,15 +1,15 @@
 import { IMoviesModel, Model } from '../usecases'
-import { Pool } from 'mysql2/promise'
+import connection from './connection'
 
 export class MoviesModel implements Model {
   private readonly connection
 
-  constructor (connection: Pool) {
+  constructor () {
     this.connection = connection
   }
 
   async get (offset: number): Promise<IMoviesModel[]> {
-    const [rows] = this.connection.execute('SELECT * FROM ghibli.movies ORDER BY original_title ASC LIMIT 20 OFFSET ?', [offset])
+    const [rows] = await this.connection.execute('SELECT * FROM db_ghibli.movies ORDER BY original_title ASC LIMIT 20 OFFSET ?', [offset])
     const movies = rows as IMoviesModel[]
     return movies
   }
