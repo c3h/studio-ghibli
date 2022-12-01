9e5b3ce1027cc776f10130af3582a37994f334bc
Gimigliano <daniel@gimigliano.blog>
2022-12-01 11:37:06 -0300

    refactor: remove console.log and trash code

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -2,25 +2,17 @@ import { MoviesModel } from '../model/movies';
 import { IMoviesModel, Model, Service } from '../protocols';
 
 export class MoviesService implements Service {
-  // private readonly getMovies
-
-  // constructor (movies: Model) {
-  //   this.getMovies = movies
-  // }
-
-  constructor (
+  constructor(
     readonly moviesModel: Model = new MoviesModel()
   ) {}
 
   async get (offset: number = 0): Promise<IMoviesModel[]> {
     const getMovies = await this.moviesModel.get(offset);
-    await this.getAPI();
     return getMovies;
   }
 
   async getAPI (): Promise<void> {
     const result = await this.moviesModel.getAPI();
-    console.log('t1');
     await this.moviesModel.addMovies(result);
   }
 }
