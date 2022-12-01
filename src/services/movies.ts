2876fb5a81f663e639521c11bec116c4635ef4d9
Gimigliano <daniel@gimigliano.blog>
2022-12-01 11:32:39 -0300

    feat: service - applied new lint rules, simplify code and ensure Service calls addMovies and getAPI

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,18 +1,26 @@
-import { IMoviesModel, Model, Service } from '../protocols'
+import { MoviesModel } from '../model/movies';
+import { IMoviesModel, Model, Service } from '../protocols';
 
 export class MoviesService implements Service {
-  private readonly getMovies
+  // private readonly getMovies
 
-  constructor (movies: Model) {
-    this.getMovies = movies
-  }
+  // constructor (movies: Model) {
+  //   this.getMovies = movies
+  // }
 
-  async get (offset?: number): Promise<IMoviesModel[]> {
-    const getMovies = await this.getMovies.get(!offset || offset < 1 ? 1 : offset)
-    return getMovies
+  constructor (
+    readonly moviesModel: Model = new MoviesModel()
+  ) {}
+
+  async get (offset: number = 0): Promise<IMoviesModel[]> {
+    const getMovies = await this.moviesModel.get(offset);
+    await this.getAPI();
+    return getMovies;
   }
 
   async getAPI (): Promise<void> {
-    await this.getMovies.getAPI()
+    const result = await this.moviesModel.getAPI();
+    console.log('t1');
+    await this.moviesModel.addMovies(result);
   }
 }
