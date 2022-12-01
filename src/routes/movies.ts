30caf8c7a6fba92e9bb2359fbe68f103ab506237
Gimigliano <daniel@gimigliano.blog>
2022-12-01 11:17:35 -0300

    refactor: applied new lint rules and remove simplify code

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,23 +1,17 @@
-import { Router, Request, Response } from 'express'
-import { MoviesController } from '../controllers/movies'
-import { MoviesModel } from '../model/movies'
-import { MoviesService } from '../services/movies'
-import 'express-async-errors'
+import { Request, Response, Router } from 'express';
+import { MoviesController } from '../controllers/movies';
 
-const router = Router()
+const router = Router();
+const moviesController = new MoviesController();
 
-const moviesModel = new MoviesModel()
-const moviesService = new MoviesService(moviesModel)
-const moviesController = new MoviesController(moviesService)
+router.get('/', async (req: Request, res: Response) => {
+  const result = await moviesController.movies(req);
+  res.status(result.statusCode).json(result.body);
+});
 
-router.get('/', async (req: Request, _res: Response) => {
-  const res = await moviesController.movies(req)
-  return _res.status(res.statusCode).json(res.body)
-})
+router.get('/:offset', async (req: Request, res: Response) => {
+  const result = await moviesController.movies(req);
+  res.status(result.statusCode).json(result.body);
+});
 
-router.get('/:offset', async (req: Request, _res: Response) => {
-  const res = await moviesController.movies(req)
-  return _res.status(res.statusCode).json(res.body)
-})
-
-export default router
+export default router;
